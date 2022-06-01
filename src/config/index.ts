4de1fb58b091baa30a5643be826d355636ef741a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-01 14:43:54 +0800

    Create index.ts

---
+++ src/config/index.ts
@@ -0,0 +1,35 @@
+/**
+ * 测试环境的配置参数
+ */
+ const config = {
+  orgId: '1201',
+  orgName: 'INV_M80', // 组织名称
+  userName: 'yanglf10',
+  // userName: 'ex_tongxf',
+  // userName: 'gejing4',
+  language: 'zh', // 默认语言
+  workcenterCode: 'CZCPA-P',
+  workcenterName: 'workcenterName',
+  userDisplayName: '',
+  cubeBridge: {
+      serverURL: '',
+      userInfo: {
+          uid: '',
+          email: '',
+          avatar: '',
+          cn: '',
+          gender: '',
+          mobile: '',
+          fullDeptName: '',
+          departmentName: '',
+          address: '',
+          vendorId: '',
+          extra: '{"xxa":"xx"}',
+          employeenumber: '',
+          positionName: '',
+          ssoToken: ''
+      }
+  }
+}
+
+export default config
