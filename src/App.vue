29dbffad8d8ccdccd64b27fa7217b2c611f93b8c
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-01 14:36:09 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <router-view v-slot="{ Component }">
+      <transition
+        name="slide"
+        mode="out-in"
+        enter-active-class="slideInRight"
+        leave-active-class="slideOutLeft">
+        <keep-alive v-if="$route.meta.keepAlive">
+          <component :is="Component"></component>
+        </keep-alive>
+        <component v-else :is="Component"></component>
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import {appStore} from './store'
+export default {
+  setup() {
+    const store = appStore()
+    store.initApp()
+    return {}
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
