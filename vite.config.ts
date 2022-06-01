96a32ccbf093a2bd47187a7a5a7e736bb39a8424
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-01 14:31:27 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteMockServe(),
+    styleImport({
+      resolves: [VantResolve()],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
+  base: './', // 设置打包路径
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    open: false,
+    cors: true, // 允许跨域
+    proxy: {
+      '/api/public': {
+        target: 'http://localhost.api.com',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+})
