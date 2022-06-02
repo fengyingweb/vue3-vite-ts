31af22a4cdaedf9e8a758efba498687e9143f3eb
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-02 16:16:23 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 
 const Home = ()=> import('../views/Home.vue')
 const MaterialDelivery = ()=> import('../views/materialDelivery.vue')
+const Register = ()=> import('../views/Register.vue')
 
 const routes = [
   {
@@ -16,6 +17,12 @@ const routes = [
     component: MaterialDelivery,
     meta: {title: '呼叫1', keepAlive: false}
   },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    meta: {title: '用户注册', keepAlive: false}
+  }
 ]
 
 const router = createRouter({
