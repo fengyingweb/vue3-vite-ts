1e699adacaa62f9ac5e2451f22f4f2661f1863a4
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-07-14 14:16:29 +0800

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,19 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router';
 
-const Home = ()=> import('../views/Home.vue')
-const MaterialDelivery = ()=> import('../views/materialDelivery.vue')
-const Register = ()=> import('../views/Register.vue')
+const Home = ()=> import('../views/Home.vue');
+const MaterialDelivery = ()=> import('../views/materialDelivery.vue');
+const Register = ()=> import('../views/Register.vue');
+const Login = ()=> import('../views/Login.vue');
+const Category = ()=> import('../views/Category.vue');
+const ShopCar = ()=> import('../views/ShopCar.vue');
+const Members = ()=> import('../views/Members.vue');
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-    meta: {title: '首页', keepAlive: true}
+    meta: {title: '商城首页', keepAlive: false}
   },
   {
     path: '/materialDelivery',
@@ -22,6 +26,30 @@ const routes = [
     name: 'Register',
     component: Register,
     meta: {title: '用户注册', keepAlive: false}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {title: '用户登录', keepAlive: false}
+  },
+  {
+    path: '/category',
+    name: 'Category',
+    component: Category,
+    meta: {title: '商品分类', keepAlive: false}
+  },
+  {
+    path: '/shopcar',
+    name: 'ShopCar',
+    component: ShopCar,
+    meta: {title: '购物车', keepAlive: false}
+  },
+  {
+    path: '/members',
+    name: 'Members',
+    component: Members,
+    meta: {title: '会员中心', keepAlive: false}
   }
 ]
 
