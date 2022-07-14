10673e6e0c0d1be558d081a16760e55a09851300
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-07-14 17:48:56 +0800

    Add files via upload

--- src/http/mall.ts
+++ src/http/mall.ts
@@ -1,10 +1,29 @@
 import request from './request'
 import {User} from '../interface/index'
 
+// 注册
 export function register(params: User) {
   return request({
     url: '/api/user/register',
     method: 'post',
     data: params
+  })
+}
+
+// 登录
+export function login(params: User) {
+  return request({
+    url: '/api/user/login',
+    method: 'post',
+    data: params
+  })
+}
+
+// 获取首页商品信息
+export function queryAllGoodsIndex() {
+  return request({
+    url: '/api/goods/index',
+    method: 'get',
+    params: {}
   })
 }
