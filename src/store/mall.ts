f1e3ae32705ed0d11ede1567a2faca10420a489d
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-07-14 17:50:51 +0800

    Add files via upload

--- src/store/mall.ts
+++ src/store/mall.ts
@@ -1,11 +1,19 @@
 import {defineStore} from 'pinia'
-import {register} from '../http/mall'
+import {register, login, queryAllGoodsIndex} from '../http/mall'
 import {User} from '../interface/index'
 export const mallStore = defineStore('mall', {
   state: ()=> {
-    return {}
+    return {
+      bannerLists: [], // banner列表
+      indexCategory: [], // grid列表
+      adBanner: '', // 广告图片
+    }
   },
-  getters: {},
+  getters: {
+    bannerListsGetters: (state)=> state.bannerLists,
+    indexCategoryGetters: (state)=> state.indexCategory,
+    adBannerGetters: (state)=> state.adBanner
+  },
   actions: {
     async goRegister(userInfo: User) {
       let params = {
@@ -14,7 +22,30 @@ export const mallStore = defineStore('mall', {
       try {
         const res = await register(params)
         return res
-      } catch (err) {}
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    
+    async goLogin(userInfo: User) {
+      const params = {
+        ...userInfo
+      }
+      try {
+        const res = await login(params)
+        return res
+      } catch (err) {
+        console.log(err)
+      }
+    },
+
+    async getAllGoodsIndex() {
+      try {
+        const res = await queryAllGoodsIndex();
+        return res
+      } catch(err) {
+        console.log(err)
+      }
     }
   }
 })
