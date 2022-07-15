cfc6da53e7e48b157b3cf972dc184d49702955f8
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-07-15 18:12:07 +0800

    Add files via upload

--- src/http/mall.ts
+++ src/http/mall.ts
@@ -1,5 +1,5 @@
 import request from './request'
-import {User} from '../interface/index'
+import {User, CategorySubParams, CategorySubLimitParams, DetailParams} from '../interface/index'
 
 // 注册
 export function register(params: User) {
@@ -25,5 +25,41 @@ export function queryAllGoodsIndex() {
     url: '/api/goods/index',
     method: 'get',
     params: {}
+  })
+}
+
+// 获取商品大类列表
+export function queryCategoryList() {
+  return request({
+    url: '/api/goods/getCategoryList',
+    method: 'get',
+    params: {}
+  })
+}
+
+// 获取商品小类数据
+export function queryCategorySubList(params:CategorySubParams) {
+  return request({
+    url: '/api/goods/getCategorySubList',
+    method: 'post',
+    data: params
+  })
+}
+
+// 根据商品类别获取商品列表,分页查询
+export function queryGoodsListByCategorySubID(params:CategorySubLimitParams) {
+  return request({
+    url: '/api/goods/getGoodsListByCategorySubID',
+    method: 'post',
+    data: params
+  })
+}
+
+// 获取商品详情信息
+export function queryDetailGoodsInfo(params: DetailParams) {
+  return request({
+    url: '/api/goods/getDetailGoodsInfo',
+    method: 'post',
+    data: params
   })
 }
