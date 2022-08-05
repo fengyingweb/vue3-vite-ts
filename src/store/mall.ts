15edbe2924ccf8bc83ac8623fd659f7193c5db5d
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-08-05 17:55:42 +0800

    Add files via upload

--- src/store/mall.ts
+++ src/store/mall.ts
@@ -1,6 +1,7 @@
 import {defineStore} from 'pinia'
 import {register, login, queryAllGoodsIndex} from '../http/mall'
 import {User} from '../interface/index'
+import '../mock'
 export const mallStore = defineStore('mall', {
   state: ()=> {
     return {
