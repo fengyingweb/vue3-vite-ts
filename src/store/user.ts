e7c8d5ed58f6f77d89ca0f4357922f2cbac4286c
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-11-30 17:57:03 +0800

    Add files via upload

---
+++ src/store/user.ts
@@ -0,0 +1,17 @@
+import {defineStore} from 'pinia'
+
+const userStore = defineStore('user', {
+  state() {
+    return {
+      userInfo: sessionStorage.getItem('userInfo') ? JSON.parse(sessionStorage.getItem('userInfo') as string) : null
+    }
+  },
+  actions: {
+    setUserInfo(playLoad: {userId: string, userName: string}) {
+      this.userInfo = {
+        ...playLoad
+      }
+    }
+  }
+})
+export default userStore
