18266f6de4ae54b344eb3f3c276745358559e2a8
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-12-22 13:49:51 +0800

    Add files via upload

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -27,3 +27,34 @@ export interface CategorySubLimitParams {
 export interface DetailParams {
   goodsId: string;
 }
+
+export interface AddShopCarParams {
+  goodsId: string;
+  name: string;
+  price: number;
+  image: string;
+  goodsNums: number;
+  userId: string;
+}
+
+export interface QueryShopCarParams {
+  id: string|number
+}
+
+export interface UpdateShopCarParams {
+  goodsId: string;
+  userId: string;
+  goodsNums: number;
+}
+
+export interface DeleteCarParams {
+  shopCarIdList: string[]
+}
+
+export interface GoodsDetail {
+  ID: string;
+  NAME: string;
+  PRESENT_PRICE: number;
+  IMAGE1: string;
+  [key: string]: any
+}
