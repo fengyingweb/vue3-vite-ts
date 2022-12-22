3710f87353526a7203bb973251c4ee8b289bad6a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-12-22 13:50:31 +0800

    Add files via upload

--- src/store/shopCar.ts
+++ src/store/shopCar.ts
@@ -1,4 +1,11 @@
 import {defineStore} from 'pinia'
+import { addShopCar, queryShopCar, updateShopCarNums, batchDeleteCar} from '../http/mall';
+import {
+  AddShopCarParams,
+  QueryShopCarParams,
+  UpdateShopCarParams,
+  DeleteCarParams
+} from '../interface/index'
 
 const shopCarStore = defineStore('shopCar', {
   state: ()=> {
@@ -8,9 +15,43 @@ const shopCarStore = defineStore('shopCar', {
     }
   },
   getters: {
+    shopCarListG: (state) => state.shopCarList,
     shopCarCountG: (state) => state.shopCarCount
   },
-  actions: {}
+  actions: {
+    async addCar(params: AddShopCarParams) {
+      try {
+        let res = await addShopCar(params)
+        return res
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async getShopCar(params: QueryShopCarParams) {
+      try {
+        let res = await queryShopCar(params)
+        return res
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async updateNums(params: UpdateShopCarParams) {
+      try {
+        let res = await updateShopCarNums(params)
+        return res
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async deleteCar(params: DeleteCarParams) {
+      try {
+        let res = await batchDeleteCar(params)
+        return res
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 })
 
 export default shopCarStore
