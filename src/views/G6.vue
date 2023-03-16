a87650c7f364230a0249a691bc4fce14df340e5d
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2023-03-16 17:36:50 +0800

    Add files via upload

--- src/views/G6.vue
+++ src/views/G6.vue
@@ -81,6 +81,47 @@ export default {
 
     let graph2 = null
     const initGraph2 = async ()=> {
+      G6.registerEdge(
+        'circle-running',
+        {
+          afterDraw(cfg, group) {
+            // get the first shape in the group, it is the edge's path here=
+            const shape = group.get('children')[0];
+            // the start position of the edge's path
+            const startPoint = shape.getPoint(0);
+
+            // add red circle shape
+            const circle = group.addShape('circle', {
+              attrs: {
+                x: startPoint.x,
+                y: startPoint.y,
+                fill: '#1890ff',
+                r: 3,
+              },
+              name: 'circle-shape',
+            });
+
+            // animation for the red circle
+            circle.animate(
+              (ratio) => {
+                // the operations in each frame. Ratio ranges from 0 to 1 indicating the prograss of the animation. Returns the modified configurations
+                // get the position on the edge according to the ratio
+                const tmpPoint = shape.getPoint(ratio);
+                // returns the modified configurations here, x and y here
+                return {
+                  x: tmpPoint.x,
+                  y: tmpPoint.y,
+                };
+              },
+              {
+                repeat: true, // Whether executes the animation repeatly
+                duration: 3000, // the duration for executing once
+              },
+            );
+          },
+        },
+        'cubic', // extend the built-in edge 'cubic'
+      )
       const response = await fetch('https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json');
       const resData = await response.json()
       const nodes = resData.nodes
@@ -136,6 +177,7 @@ export default {
           }
         },
         defaultEdge: {
+          type: 'circle-running',
           style: {
             stroke: "#f56"
           },
