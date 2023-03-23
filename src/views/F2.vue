65dbeab3e824c92daebb80a15a66b7889158effe
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2023-03-23 10:34:04 +0800

    Add files via upload

--- src/views/F2.vue
+++ src/views/F2.vue
@@ -1,7 +1,7 @@
 <script lang="jsx">
 import { toRaw } from 'vue';
 import Canvas from '@antv/f2-vue';
-import { Chart, Interval, Axis } from '@antv/f2';
+import { Chart, Interval, Axis, Legend, Tooltip } from '@antv/f2';
 // @ts-ignore
 import MNavBar from '@/components/navBar'
 
@@ -19,12 +19,45 @@ const data2 = [
   { genre: 'Shooter', sold: 50 },
   { genre: 'Other', sold: 50 },
 ];
+const data3 = [
+  {
+    name: '长津湖',
+    percent: 40,
+    a: '100',
+  },
+  {
+    name: '我和我的父辈',
+    percent: 20,
+    a: '100',
+  },
+  {
+    name: '失控玩家',
+    percent: 18,
+    a: '100',
+  },
+  {
+    name: '宝可梦',
+    percent: 15,
+    a: '100',
+  },
+  {
+    name: '峰爆',
+    percent: 5,
+    a: '100',
+  },
+  {
+    name: '其他',
+    percent: 2,
+    a: '100',
+  },
+];
 export default {
   name: 'F2',
   data() {
     return {
       year: '2021',
       chartData: data1,
+      polarData: data3
     };
   },
   mounted() {
@@ -34,7 +67,7 @@ export default {
     }, 5000);
   },
   render() {
-    const { year, chartData } = this;
+    const { year, chartData, polarData } = this;
     return (
       <div className="wrapper">
         <MNavBar/>
@@ -42,9 +75,66 @@ export default {
           <div className="container1-f2">
             <Canvas pixelRatio={window.devicePixelRatio}>
               <Chart data={toRaw(chartData)}>
+                <Tooltip showTooltipMarker={true} />
                 <Axis field="genre" />
                 <Axis field="sold" />
-                <Interval x="genre" y="sold" color="genre" />
+                <Interval
+                  x="genre"
+                  y="sold"
+                  color="genre"
+                  animation={{
+                    update: {
+                      easing: 'linear',
+                      duration: 200,
+                      property: ['x', 'y', 'width', 'height'],
+                    },
+                  }}
+                  selection={{
+                    selectedStyle: (record) => {
+                      const { xMin, xMax } = record;
+                      const width = xMax - xMin;
+                      const offset = width * 0.2;
+                      return {
+                        x: xMin - offset,
+                        width: width + offset * 2,
+                        fillOpacity: 1,
+                      };
+                    },
+                    unSelectedStyle: {
+                      fillOpacity: 0.4,
+                    },
+                  }}
+                />
+              </Chart>
+            </Canvas>
+          </div>
+          <div className="container2-f2">
+            <Canvas pixelRatio={window.devicePixelRatio}>
+              <Chart
+                data={toRaw(polarData)}
+                coord={{
+                  transposed: true,
+                  type: 'polar',
+                }}>
+                <Legend position="right" />
+                <Interval
+                  x="a"
+                  y="percent"
+                  adjust="stack"
+                  color={{
+                    field: 'name',
+                    range: ['#1890FF', '#13C2C2', '#2FC25B', '#FACC14', '#F04864', '#8543E0'],
+                  }}
+                  selection={{
+                    selectedStyle: (record) => {
+                      const { yMax, yMin } = record;
+                      return {
+                        // 半径放大 1.1 倍
+                        r: (yMax - yMin) * 1.1,
+                      };
+                    }
+                  }}
+                />
               </Chart>
             </Canvas>
           </div>
@@ -68,5 +158,9 @@ export default {
   .container1-f2 {
       width: 100%;
       height: 300px;
+  }
+  .container2-f2 {
+    width: 100%;
+    height: 500px;
   }
 </style>
