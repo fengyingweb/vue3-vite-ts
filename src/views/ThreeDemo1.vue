6dec4808f155970d992cb98792a53ad3ab10512c
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2023-03-24 17:40:31 +0800

    Add files via upload

--- src/views/ThreeDemo1.vue
+++ src/views/ThreeDemo1.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="wrapper">
     <m-nav-bar></m-nav-bar>
+    <section class="spt-wrapper">
+      <div>两帧渲染时间间隔(毫秒)：<span class="spt">{{spt}}</span></div>
+      <div>帧率FPS：<span class="spt">{{1000/spt}}</span></div>
+    </section>
     <section ref="canvasRef" class="con-section">
     </section>
   </div>
@@ -11,11 +15,12 @@ import { onMounted, ref } from 'vue'
 // @ts-ignore
 import mNavBar from '@/components/navBar'
 import * as THREE from 'three'
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 // console.log(OrbitControls)
 // console.log(THREE)
 
 const canvasRef = ref(null)
+const spt = ref(0)
 
 const initThree = ()=> {
   // 创建3D场景对象
@@ -28,7 +33,9 @@ const initThree = ()=> {
 
   // 创建一个材质对象Material MeshLambertMaterial :网格漫反射材质
   const material = new THREE.MeshLambertMaterial({
-    color: 0xff5566 // 设置材质颜色
+    color: 0xff5566, // 设置材质颜色
+    transparent: true,//开启透明
+    opacity: 0.5,//设置透明度
   })
   // console.log(material)
 
@@ -37,18 +44,26 @@ const initThree = ()=> {
   // console.log(mesh)
 
   // 设置网格模型在三维空间中的位置坐标，默认是坐标原点
-  mesh.position.set(0, 10, 0)
+  mesh.position.set(0, 0, 0)
 
   // 把网格模型mesh添加到三维场景scene中
   scene.add(mesh)
+
+  // AxesHelper：辅助观察的坐标系
+  const axesHelper = new THREE.AxesHelper(120)
+  scene.add(axesHelper)
 
   /**
    * 光源设置
   **/
   // 点光源
   const point = new THREE.PointLight(0xffffff)
-  point.position.set(400,200,300) // 点光源位置
+  point.position.set(400, 200, 300) // 点光源位置
   scene.add(point) // 点光源添加到场景中
+
+  // 光源辅助观察
+  const pointLightHelper = new THREE.PointLightHelper(point, 10);
+  scene.add(pointLightHelper)
 
   // 环境光
   const ambient = new THREE.AmbientLight(0x444444)
@@ -75,7 +90,7 @@ const initThree = ()=> {
   // 相机观察目标指向Threejs 3D空间中某个位置 .lookAt()
   // camera.lookAt(0, 0, 0) // 坐标原点
   // camera.lookAt(0, 50, 0) // y轴上位置50
-  camera.lookAt(mesh.position) // 指向物体mesh的位置
+  camera.lookAt(mesh.position) // 指向物体mesh的位置或者scene场景的位置
 
   // 创建渲染器对象
   const renderer = new THREE.WebGLRenderer()
@@ -88,13 +103,20 @@ const initThree = ()=> {
   renderer.setSize(width, height)
 
   // 设置背景颜色
-  renderer.setClearColor(0xb9d3ff,1)
+  renderer.setClearColor(0xb9d3ff, 1)
+
+  // 执行渲染操作 .render()
+  // renderer.render(scene, camera)
 
   // 把canvas画布插入到HTML元素中
   canvasRef.value.appendChild(renderer.domElement)
 
-  // 执行渲染操作 .render()
-  renderer.render(scene, camera)
+  // 设置相机控件轨道控制器 OrbitControls
+  const controls = new OrbitControls(camera, renderer.domElement)
+
+  controls.addEventListener('change', ()=> {
+    renderer.render(scene, camera)
+  })
 
   window.addEventListener('resize', ()=> {
     const width = canvasRef.value.clientWidth
@@ -103,6 +125,19 @@ const initThree = ()=> {
     camera.updateProjectionMatrix()
     renderer.setSize(width, height)
   }, false)
+
+  // 渲染循环
+  const clock = new THREE.Clock()
+
+  // 动画渲染
+  function animateRender() {
+    spt.value = clock.getDelta()*1000 //毫秒
+    renderer.render(scene, camera) //执行渲染操作
+    mesh.rotateY(0.01) //每次绕y轴旋转0.01弧度
+    requestAnimationFrame(animateRender) //请求再次执行渲染函数render，渲染下一帧
+  }
+
+  animateRender()
 }
 
 onMounted(()=> {
@@ -115,6 +150,18 @@ onMounted(()=> {
       width: 100%;
       min-height: 100vh;
       background: #fff;
+  }
+  .spt-wrapper {
+    padding: 32px;
+    color: #333;
+    &> div {
+        width: 100%;
+        line-height: 36px;
+    }
+  }
+  .spt {
+    display: inline-block;
+    width: 300px;
   }
   .con-section {
       width: 100%;
