55739fe4c7336f2c53bb635ab7ba08cb45c4a6eb
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2023-12-13 15:31:27 +0800

    Add files via upload

---
+++ src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <m-nav-bar></m-nav-bar>
+    <section class="con-section">
+      404 Not Found Page
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+// @ts-ignore
+import mNavBar from '@/components/navBar'
+onMounted(()=> {
+})
+</script>
+
+<style lang="less" scoped>
+  .wrapper {
+      width: 100%;
+      min-height: 100vh;
+      background: #fff;
+  }
+</style>
